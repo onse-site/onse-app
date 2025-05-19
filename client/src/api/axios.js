bed6625cde767c9ca95fc146b82dc7db23b554d3
ABDELJALIL LAFKIR <ahmedjalallafkir@gmail.com>
2025-05-19 15:36:58 +0100

    fix the backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  /*  baseURL: "https://onse-app.onrender.com/", */
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://onse-app.onrender.com/",
+  /* baseURL: "http://localhost:3000/", */
   withCredentials: true,
 });
 
